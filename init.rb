39c08d73884b173cac5c78b85714ccd90e22c464
apuimedo <devnull@localhost>
2012-06-11 15:52:30 +0000

    Bumped up version number.

--- init.rb
+++ init.rb
@@ -7,7 +7,7 @@ Redmine::Plugin.register :redmine_gemavatar do
   name 'Redmine Gemavatar plugin'
   author 'Antoni Segura Puimedon'
   description 'Fetches gemalto avatars from LDAP'
-  version '0.0.1'
+  version '1.0.0'
   url ''
   author_url 'http://celebdor.com'
   settings :default => {}, :partial => 'settings/gemavatar'
